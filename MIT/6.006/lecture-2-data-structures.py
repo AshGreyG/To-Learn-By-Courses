421b1752d11c99cadd01c41b121cb3ee92bc28da
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-06-02 12:20:46 +0800

    :sparkles: today (2025-06-02) learn the linked list implementation of sequence

--- MIT/6.006/lecture-2-data-structures.py
+++ MIT/6.006/lecture-2-data-structures.py
@@ -133,7 +133,7 @@ class ArraySequence(Generic[T]) :
             The element at index `i`, which is the deleted element.
         """
         n = len(self)
-        A : list[T | None] = [None] * (n + 1)
+        A : list[T | None] = [None] * (n - 1)
         self._copy_forward(0, i, A, 0)
         x = self.A[i]
         self._copy_forward(i + 1, n - i - 1, A, i)
@@ -145,9 +145,134 @@ class ArraySequence(Generic[T]) :
     def insert_last(self, x : T) -> None :  self.insert_at(len(self), x)
     def delete_last(self) -> T :            return self.delete_at(len(self) - 1)
 
+# A **Linked list** stores each item in a node, `node`, a constant-sized container
+# with two properties: `node.item` storing the `item`, and `node.next` storing the
+# memory address of the node containing the next item in the sequence.
+
+class LinkedListNode(Generic[T]) :
+    def __init__(self, x : T) -> None :
+        self.item : T = x
+        self.next : LinkedListNode[T] | None = None
+    
+    def later_node(self, i : int) -> LinkedListNode[T] : # O(i)
+        """
+        This method gets the `i`-th node after this node, if the node is None,
+        it will assert the error.
+
+        Args:
+            i: The nodes amount after this node.
+        Returns:
+            The `i`-th node after this node.
+        """
+        if i == 0 :
+            return self
+        assert self.next
+        return self.next.later_node(i - 1)
+
+# Such data structure are sometimes called **pointer-based** or **linked** and
+# are much more flexible than array-based data structures because their 
+# constituent items can be stored **anywhere** in memory.
+
+# A linked list stores the address of the node storing the first element
+# of the list called the **head** of the list, along with linked list's size.
+# It's easy to add an item after another item in the list, simply by changing
+# some addresses. Adding a new item at the front (head) of the list takes
+# O(1) time. However, the only way to find the i-th item in the sequence is
+# to step through the items one-by-one
+
+class LinkedListSequence(Generic[T]) :
+    def __init__(self) -> None :
+        self.head : LinkedListNode[T] | None = None
+        self.size : int = 0
+
+    def __len__(self) -> int :
+        return self.size
+
+    def __iter__(self) -> T :
+        node : LinkedListNode = self.head
+        while node != None :
+            yield node.item
+            node = node.next
+
+    def build(self, X : list[T]) -> None :
+        for a in reversed(X) :
+            self.insert_first(a)
+
+    def get_at(self, i : int) -> T :    # O(i)
+        node = self.head.later_node(i)
+        return node.item
+
+    def set_at(self, i : int, x : T) -> None :
+        node = self.head.later_node(i)
+        node.item = x
+
+    def insert_first(self, x : T) -> None : # O(1)
+        """
+        This method inserts the element `x` at the front of head.
+
+        Args:
+            x: The inserted element.
+        """
+        new_node = LinkedListNode(x)
+        new_node.next = self.head
+        self.head = new_node
+        self.size += 1
+
+    def delete_first(self) -> T :   # O(1)
+        """
+        This method deletes the first element of linked list and returns it.
+
+        Returns:
+            x: The first element of original linked list.
+        """
+        x = self.head.item
+        self.head = self.head.next
+        self.size -= 1
+        return x
+
+    def insert_at(self, i : int, x : T) -> None :   # O(i)
+        """
+        This method inserts the element `x` at the index `i` of the linked list.
+
+        Args:
+            i: The inserted index, new element will at `i` in the new linked list.
+            x: The inserted item.
+        """
+        if i == 0 :
+            self.insert_first(x)
+            return
+        new_node = LinkedListNode(x)
+        insert_node = self.head.later_node(i - 1)
+        new_node.next = insert_node.next
+        insert_node.next = new_node
+        self.size += 1
+
+    def delete_at(self, i : int) -> T : # O(i)
+        """
+        This method deletes the element at the index `i` of the linked list.
+        """
+        if i == 0 :
+            return self.delete_first()
+        delete_node = self.head.later_node(i - 1)
+        x = delete_node.next.item
+        delete_node.next = delete_node.next.next
+        self.size -= 1
+        return x
+
 if __name__ == "__main__" :
+    # ArraySequence simple tests
+
     array_seq = ArraySequence()
     array_seq.build([1, 2, 3, 4])
     print(array_seq.get_at(3))  # 4, array_seq: [1, 2, 3, 4]
     array_seq.set_at(1, 3)
     print(array_seq)            # array_seq: [1, 3, 3, 4]
+    array_seq.insert_at(2, "3")
+    print(array_seq)            # array_seq: [1, 3, '3', 3, 4]
+    array_seq.delete_at(2)
+    print(array_seq)            # array_seq: [1, 3, 3, 4]
+    array_seq.insert_first(3)
+    print(array_seq)            # array_seq: [3, 1, 3, 3, 4]
+
+    for e in array_seq :
+        print(e)
